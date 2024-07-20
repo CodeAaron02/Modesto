4c9be4abdbbe679e1f4d86a982bab0dad86fe28d
Justin <delarocaaaron@gmail.com>
2024-07-20 20:49:02 +0800

    Edited

--- JAVASCRIPT/guest.js
+++ JAVASCRIPT/guest.js
@@ -11,24 +11,7 @@ function menutoggle() {
   }
 }
 
-// SCROLL UP NAVBAR
-
-let lastScrollTop = 0;
-const navbar = document.getElementById("navbar");
-
-window.addEventListener("scroll", () => {
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop) {
-    // Scrolling down
-    navbar.classList.add("hidden");
-  } else {
-    // Scrolling up
-    navbar.classList.remove("hidden");
-  }
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
-});
-
-//
+// LOG IN FORM
 
 function openLoginForm() {
   document.getElementById("loginForm").style.display = "block";
