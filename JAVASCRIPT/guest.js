690cbfd6cdffc81c67ab80d73546ef31c37276f3
Justin <delarocaaaron@gmail.com>
2024-07-20 14:47:21 +0800

    Edited

--- JAVASCRIPT/guest.js
+++ JAVASCRIPT/guest.js
@@ -11,6 +11,23 @@ function menutoggle() {
   }
 }
 
+// SCROLL UP NAVBAR
+
+let lastScrollTop = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down
+    navbar.classList.add("hidden");
+  } else {
+    // Scrolling up
+    navbar.classList.remove("hidden");
+  }
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+});
+
 //
 
 function openLoginForm() {
@@ -80,23 +97,6 @@ const fetchBookedDates = async () => {
 
 // Fetch booked dates and render calendar on page load
 document.addEventListener("DOMContentLoaded", fetchBookedDates);
-
-// SCROLL UP NAVBAR
-
-let lastScrollTop = 0;
-const navbar = document.getElementById("navbar");
-
-window.addEventListener("scroll", () => {
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop) {
-    // Scrolling down
-    navbar.classList.add("hidden");
-  } else {
-    // Scrolling up
-    navbar.classList.remove("hidden");
-  }
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
-});
 
 // IMAGE FULLSCREEN
 document.querySelectorAll(".gallery-wrapper img").forEach((img) => {
